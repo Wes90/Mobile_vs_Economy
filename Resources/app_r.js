7132d08f7c79cbcef915e4ca8c4964209d1c6d2c
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-30 13:06:30 -0700

    I don't need to create a ratio, going to create a scatter plot

--- Resources/app_r.js
+++ Resources/app_r.js
@@ -1,8 +1,16 @@
-// Mobile Penetration vs banking services
+// Mobile Penetration vs % of population >15 YO w/ banking services
 // Ratio bar chart
 
 // Create a function to pull in the json data
 // Using 'd3.json' gather the metadata for each sample
-d3.json('mobile_subscriptions_clean.json').then((samples)=>{
-    var id=mobile_subscriptions_clean.Country%20name;
-    console.log(mobile_subscriptions_clean.metadata);
+d3.json('mergerd_tables.json').then((mergerd_tables)=>{
+    var id=mergerd_tables.Country%20name;
+    console.log(mergerd_tables.metadata);
+
+    var select=d3.selectAll('#ZZZZZZt');;
+
+    country.forEach((v)=>{
+        select.append('option').text(v).property("value",v);
+    })   
+
+    
