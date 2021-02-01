b5cc87359157dcef0ab732c98c118384a52de813
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-31 20:36:46 -0700

    updated references for new .csv

--- Resources/app_r.js
+++ Resources/app_r.js
@@ -1,12 +1,13 @@
-// Mobile Penetration vs % of population >15 YO w/ banking services
+// % of Total Population with Mobile Penetration vs % of population >15 YO w/ banking services
 // Ratio bar chart
 
 // Create a function to pull in the json data
 // Using 'd3.json' gather the metadata for each sample
-d3.json('Mergerd_table.json').then((Mergerd_table)=>{
-    var id=Mergerd_table.Country%20name;
-    console.log(Mergerd_table.metadata);
+d3.json('merged_normalized_data.json').then((merged_normalized_data)=>{
+    var id=merged_normalized_data.Country%20name;
+    console.log(merged_normalized_data.metadata);
 
+    //select the drop down menu
     var select=d3.selectAll('#ZZZZZZt');;
 
     Country%20name.forEach((v)=>{
@@ -21,8 +22,8 @@ d3.json('Mergerd_table.json').then((Mergerd_table)=>{
 // Update metadata
 function updateMeta(Country%20name){
     // read in the data file 
-    d3.json('mergerd_tables.json').then((Mergerd_table)=>{
-        var meta=Mergerd_table.metadata;
+    d3.json('merged_normalized_data.json').then((merged_normalized_data)=>{
+        var meta=merged_normalized_data.metadata;
         // Take the country and filter the variable meta
         var results = meta.filter(newData => newData.Country%20name == Country%20name)[0]
         // With the filtered results post them in the #sample-metadata div
