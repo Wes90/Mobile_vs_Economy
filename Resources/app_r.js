b922bc7934d91548a10a59b9c89cf79126d989a9
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-31 21:14:38 -0700

    normalized columns

--- Resources/app_r.js
+++ Resources/app_r.js
@@ -4,17 +4,17 @@
 // Create a function to pull in the json data
 // Using 'd3.json' gather the metadata for each sample
 d3.json('merged_normalized_data.json').then((merged_normalized_data)=>{
-    var id=merged_normalized_data.Country%20name;
+    var id=merged_normalized_data.Country_name;
     console.log(merged_normalized_data.metadata);
 
     //select the drop down menu
     var select=d3.selectAll('#ZZZZZZt');;
 
-    Country%20name.forEach((v)=>{
+    Country_name.forEach((v)=>{
         select.append('option').text(v).property("value",v);
     })   
 
-    firstcountry=Country%20name[0]
+    firstcountry=Country_name[0]
     makePlot(firstcountry)
     updateMeta(firstcountry)
 })   
@@ -35,3 +35,26 @@ function updateMeta(Country%20name){
         })
     })
 }
+
+// Define the source of the data and make the charts
+function makePlot(Country%20name){
+    d3.json('merged_normalized_data.json').then((merged_normalized_data)=>{
+        // create an array
+        var country=merged_normalized_data.samples;
+        var testNum=samples.map(row=>row.id).indexOf(testId);
+
+        // Define the bar chart
+        var otuValueTen=samples.map(row=>row.sample_values);
+        var otuValueTen=otuValueTen[testNum].slice(0,10).reverse();
+        var otuIdTen=samples.map(row=>row.otu_ids);
+        var otuIdTen=otuIdTen[testNum].slice(0,10);
+        var otuLabelTen=samples.map(row=>row.otu_labels); 
+        var otuLabelTen=otuLabelTen[testNum].slice(0,10); 
+        var trace={
+            x: otuValueTen,
+            y: otuIdTen.map(r=>`UTO ${r}`),
+            text: otuLabelTen,
+            type:'bar',
+            orientation:'h'
+        }
+        Plotly.newPlot('bar',[trace]);
