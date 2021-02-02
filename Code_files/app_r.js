7cef524fcc3f90cb56e14cdc612de90f42400cca
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-02-01 22:20:56 -0700

    working graph

--- Code_files/app_r.js
+++ Code_files/app_r.js
@@ -15,87 +15,96 @@ d3.csv('Resources/merged_normalized_data.csv').then((merged_normalized_data)=>{
     Country_name=[]
     merged_normalized_data.forEach((v)=>{
         Country_name.push(v.Country_name);
-        console.log(v.Country_name)
+        // console.log(v.Country_name)
+        select.append('option').text(v.Country_name).property("value",v.Country_name);
     })   
 
     firstcountry=Country_name[0]
     makePlot(firstcountry)
-    updateMeta(firstcountry)
 })   
-
-// Update metadata
-function updateMeta(Country_name){
-    // read in the data file 
-    d3.csv('Resources/merged_normalized_data.csv').then((merged_normalized_data)=>{
-        var meta=merged_normalized_data.metadata;
-        // Take the country and filter the variable meta
-        var results = meta.filter(newData => newData.Country_name == Country_name)[0]
-        // With the filtered results post them in the #sample-metadata div
-        var metadata=d3.selectAll('#sample-metadata');;    
-        metadata.html("");
-        // Show all the meta data
-        Object.entries(results).forEach(([key, value]) => {
-            metadata.append("p").text(`${key}: ${value}`);
-        })
-    })
-}
 
 // Define the source of the data and make the charts
 function makePlot(country){
     d3.csv('Resources/merged_normalized_data.csv').then((merged_normalized_data)=>{
         // create an array
-        var merged_normalized_data=merged_normalized_data.merged_normalized_data;
-        var countryName=merged_normalized_data.map(row=>row.Country_name).indexOf(country);
+        // var merged_normalized_data=merged_normalized_data.merged_normalized_data;
+        // var countryName=merged_normalized_data.map(row=>row.Country_name).indexOf(country);
+        Country_name=[]
+        // merged_normalized_data.forEach((v)=>{
+        // Country_name.push(v.Country_name);
+        var holder = merged_normalized_data
+        holder=holder.filter(a =>a.Country_name == country)
+
 
     // Define the chart
-    var ao_2011=merged_normalized_data.map(row=>row.x2011_AO);
-    var subs_2011=merged_normalized_data.map(row=>row.x2011_SUBS);
+    var ao_2011=holder.map(row=>row.x2011_AO);
+    var subs_2011=holder.map(row=>row.x2011_SUBS_norm);
     var ao_2014=merged_normalized_data.map(row=>row.x2014_AO);
     var subs_2014=merged_normalized_data.map(row=>row.x2014_SUBS);
     var ao_2017=merged_normalized_data.map(row=>row.x2017_AO);
     var subs_2017=merged_normalized_data.map(row=>row.x2017_SUBS);
     var countryLabel=merged_normalized_data.map(row=>row.Country_name); 
+    console.log("dinosaurs", subs_2011)
+    subs_2011 = subs_2011.map((i) => Number(i));
+    ao_2011 = ao_2011.map((i) => Number(i));
+    console.log("1", ao_2011)
     var trace1={
-        x: [subs_2011, subs_2014, subs_2017],
-        y: [ao_2011, ao_2014, ao_2017], 
+        x: subs_2011,
+        y: ao_2011, 
         mode: 'markers+text',
         type: 'scatter',
         name: '2011',
-        text: data['Country_name'],
+        // text: data['Country_name'],
         // hover data = ['Country_name']
     };
     var data1=[trace1];
     var Layout={
         xaxis:{
-            range: [0,1.5],
+            range: [0,1.50],
             title: {
                 text: 'Percent of Population with Mobile Subscriptions'
             }
         },
         yaxis: {
-            range: [0,1],
+            range: [0,100],
             text: '% of population >15 YO w/ banking services'
         }
     };
-    Plotly.newPlot('scatter_country',data1,Layout); 
-    })
+    Plotly.newPlot('scatter_country',data1, Layout); 
+    // })
 }
-    //var trace2={
-        x: [x2011_SUBS],
-        y: [x2011_AO],
-        mode: 'markers+text',
-        type: 'scatter',
-        text: data['Country_name']
-    }
+
+)}
+// Review new Country
+// function optionChanged(newCountry) {
+//     // Select a new ID from the drop down menu
+//     makePlot(newId);
+// }
+    // Create a cross plot of 2011 data
+    // var ao_2011=merged_normalized_data.map(row=>row.x2011_AO);
+    // var subs_2011=merged_normalized_data.map(row=>row.x2011_SUBS);
+    // var ao_2014=merged_normalized_data.map(row=>row.x2014_AO);
+    // var subs_2014=merged_normalized_data.map(row=>row.x2014_SUBS);
+    // var ao_2017=merged_normalized_data.map(row=>row.x2017_AO);
+    // var subs_2017=merged_normalized_data.map(row=>row.x2017_SUBS);
+    // var countryLabel=merged_normalized_data.map(row=>row.Country_name); 
+
+    // var trace2={
+        // x: [subs_2011],
+        // y: [ao_2011],
+    //     mode: 'markers+text',
+    //     type: 'scatter',
+    //     text: data['Country_name']
+    // }
     
-    var data2 = [trace2]
-    var layout = {
-        xaxis: {
-            range: [0,1.5]
-        },
-        yaxis: {
-            range: [0,1]
-        },
-        title:'2011 Mobile Subscriptions vs Mobile Banking Services'
-    };
-    Plotly.newPlot('scatter2011', data2, layout);
+    // var data2 = [trace2]
+    // var layout = {
+    //     xaxis: {
+    //         range: [0,1.5]
+    //     },
+    //     yaxis: {
+    //         range: [0,1]
+    //     },
+    //     title:'2011 Mobile Subscriptions vs Mobile Banking Services'
+    // };
+    // Plotly.newPlot('scatter2011', data2, layout);
